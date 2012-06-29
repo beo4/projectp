61871dc5e137714eb5df56ad57859f4fcb53b756
beo4 <behounek@W1IT10.kathrein.de>
2012-06-29 16:14:14 +0200

    Change-Id: If04796fdccbc040d6851ec32e220d2f310c9ce46

---
+++ fdup/fdup/grails-app/taglib/twitter/bootstrap/scaffolding/PromptTagLib.groovy
@@ -0,0 +1,47 @@
+package twitter.bootstrap.scaffolding
+
+class PromptTagLib {
+
+	static namespace = "bootstrap"
+
+
+
+	def prompt = {
+		attrs ->
+		// actually imports '/app/js/bootbox.js'
+		out << g.javascript(src:"bootbox.js")
+
+		def name = attrs.name
+		def map
+		
+		attrs.onSuccess=name + "bootbox(data)"
+		attrs.onFailure =name + "bootbox(data)"
+		attrs.method = 'GET'
+		
+
+		out << g.formRemote(attrs){
+					out << '<div class="form-actions">'
+					out << '<button type="submit" class="btn btn-primary bs-prompt">'
+					out << '	<i class="icon-add icon-white"></i>'
+					out << g.message(code:"default.button.create.label", default:"Create" )
+					out << '</button>'
+					out << '</div>'
+					
+				}
+		out << g.javascript(){
+			'function ' + name + 'bootbox(e) {'+
+			'bootbox.alert(e){'+
+			'[{ '+
+                   ' "label" : "Success!",'+
+                   ' "class" : "btn-success",'+
+                   ' "callback": function() {'+
+                    '    console.log("great success");'+
+                    '}'+
+                '}'+
+			']}}'
+		}
+
+		
+
+	}
+}
